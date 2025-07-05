8509927089c2d9475b58f070e5616c7d9ab72f62
Hassan-2504 <hassanfarooq2504@gmail.com>
2025-07-05 15:55:56 +0500

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Footer = () => {
   const [mounted, setMounted] = useState(false);
